5c15f31028d6c054d469e114d8efebf1bf44041f
Elua-b <bugingoeloi@gmail.com>
2022-07-11 10:04:02 +0200

    duplicate letters function

---
+++ duplicate.py
@@ -0,0 +1,13 @@
+
+def is_isogram(string):
+    for i in string:
+        if string.count(i) > 1:
+            return True
+    return False
+
+print(is_isogram('pear'))
+
+
+print(is_isogram('apple'))
+
+   
